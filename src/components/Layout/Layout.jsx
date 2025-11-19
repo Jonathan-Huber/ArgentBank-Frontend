81691e1b5fd0b51254e86ed1f164db03130de64b
Jonathan <jonathan.mfm@gmail.com>
2025-11-19 09:58:02 +0100

    refactor: ajouter la gestion des classes spécifiques selon la page dans Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 function Layout() {
+  const { pathname } = useLocation();
+
+  const specialPages = ["/sign-in", "/profile"];
+  const mainClass = specialPages.includes(pathname) ? "bg-dark" : "";
+
   return (
     <>
       <Header />
-      <main>
+      <main className={mainClass}>
         <Outlet />
       </main>
       <Footer />
