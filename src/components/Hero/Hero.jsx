ba5eb4b465a0d7dba70193229fdae4b65941174e
Jonathan <jonathan.mfm@gmail.com>
2025-11-19 09:46:56 +0100

    feat: créer la page Home avec le composant Hero et son style

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import "./_hero.scss";
+
+function Hero({ src, srcSet, width, height, alt, title, subtitles, text }) {
+  return (
+    <div className="hero">
+      <img
+        className="hero__image"
+        src={src}
+        srcSet={srcSet}
+        sizes="100vw"
+        width={width}
+        height={height}
+        alt={alt}
+      />
+      <section className="hero__content">
+        <h2 className="sr-only">{title}</h2>
+        {subtitles.map((sub, index) => (
+          <p key={index} className="hero__subtitle">
+            {sub}
+          </p>
+        ))}
+        <p className="hero__text">{text}</p>
+      </section>
+    </div>
+  );
+}
+export default Hero;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import Hero from "../../components/Hero/Hero";
+
+import hero400 from "../../assets/images/hero-400.webp";
+import hero800 from "../../assets/images/hero-800.webp";
+import hero1200 from "../../assets/images/hero-1200.webp";
+import hero1600 from "../../assets/images/hero-1600.webp";
+
+function Home() {
+  return (
+    <Hero
+      src={hero1200}
+      srcSet={`${hero400} 400w, ${hero800} 800w, ${hero1200} 1200w, ${hero1600} 1600w`}
+      width={1200}
+      height={800}
+      alt="Une petite plante pousse dans un verre rempli de pièces de monnaie, symbolisant la croissance financière"
+      title="Promoted Content"
+      subtitles={["No fees.", "No minimum deposit.", "High interest rates."]}
+      text="Open a savings account with Argent Bank today!"
+    />
+  );
+}
+
+export default Home;
